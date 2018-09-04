0f9e4400e8319f3e3a99865588541b95016b586b
sean regan <rego363@gmail.com>
2018-09-04 13:06:37 +0100

    First commit

---
+++ Assets/VrLookWalk.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrLookWalk : MonoBehaviour {
+
+
+    public Transform vrCam;
+
+    public float toggleAngle = 30.0f;
+
+    public float speed = 3.0f;
+
+    public bool moveForward;
+
+    private bool previousTouch;
+    private int timer = 0;
+
+    private CharacterController cc;
+	// Use this for initialization
+	void Start () {
+        cc = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Space pressed");
+        }
+
+        if (timer > 100 || vrCam.eulerAngles.x >= toggleAngle&& vrCam.eulerAngles.x<90.0f)
+        {
+            moveForward = true;
+        }
+        else
+        {
+            moveForward = false;
+        }
+
+        if(moveForward)
+        {
+            Vector3 forward = vrCam.TransformDirection(Vector3.forward);
+
+            cc.SimpleMove(forward * speed);
+        }
+
+        if ((Input.GetKeyDown("space") || Input.touchCount > 0) && previousTouch == true)
+        {
+            Debug.Log(timer);
+            timer++;
+        }
+
+        if(Input.GetKeyDown("space") || Input.touchCount > 0)
+        {
+            previousTouch = true;
+        }
+        else
+        {
+            previousTouch = false;
+            timer = 0; 
+        }
+
+    }
+}
---
+++ Assets/gameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void goDevRoom()
+    {
+        Application.LoadLevel("DevRoom");
+    }
+
+    public void goBack()
+    {
+        Application.LoadLevel("Scene");
+    }
+    public void goFitzgeralds()
+    {
+        Application.LoadLevel("Fitzgerald Room");
+    }
+}
